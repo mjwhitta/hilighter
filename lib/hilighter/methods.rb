6da14d562545fe55307e6aab0a07c2395ce495b5
Miles Whittaker <mjwhitta@gmail.com>
2016-04-28 23:28:21 -0400

    Added on_rainbow and rainbow methods

--- lib/hilighter/methods.rb
+++ lib/hilighter/methods.rb
@@ -1,7 +1,63 @@
 class Hilighter
     module Methods
+        def on_rainbow
+            return self if (Hilighter.disable?)
+
+            clrs = rainbow_colors
+            out = Array.new
+            i = 0
+
+            self.chars.each do |c|
+                if (c.match(/\s/))
+                    out.push(c)
+                else
+                    out.push(c.black.send("on_#{clrs[i % clrs.length]}"))
+                    i += 1
+                end
+            end
+
+            return out.join
+        end
+
         def plain
             return self.gsub(/\e\[([0-9;]*m|K)/, "")
+        end
+
+        def rainbow_colors
+            return [
+                "red",
+                "green",
+                "yellow",
+                "blue",
+                "magenta",
+                "cyan",
+                "light_red",
+                "light_green",
+                "light_yellow",
+                "light_blue",
+                "light_magenta",
+                "light_cyan",
+            ]
+        end
+        private :rainbow_colors
+
+        def rainbow
+            return self if (Hilighter.disable?)
+
+            clrs = rainbow_colors
+            out = Array.new
+            i = 0
+
+            self.chars.each do |c|
+                if (c.match(/\s/))
+                    out.push(c)
+                else
+                    out.push(c.send(clrs[i % clrs.length]))
+                    i += 1
+                end
+            end
+
+            return out.join
         end
 
         def wrap(width = 80)
