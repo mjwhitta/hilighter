211e7dde3d832bba7577f9a7c5e4d79319e224f5
Miles Whittaker <mj@whitta.dev>
2020-02-09 21:00:27 -0500

    HexToXterm256 is now exported

--- hilighter.go
+++ hilighter.go
@@ -2,6 +2,7 @@ package hilighter
 
 import (
 	"errors"
+	"math"
 	"runtime"
 	"strconv"
 	"strings"
@@ -15,7 +16,109 @@ func Disable(b bool) {
 // Hex will take a hex color code and add the appropriate ANSI color
 // codes to the provided string.
 func Hex(hex string, str string) string {
-	return colorize(hexToXterm256(hex), str)
+	return colorize(HexToXterm256(hex), str)
+}
+
+// HexToXterm256 will convert hex to xterm-256 8-bit value
+// https://stackoverflow.com/questions/11765623/convert-hex-hex_to_256bitto-closest-x11-color-number
+func HexToXterm256(hex string) string {
+	var hasKey bool
+	if _, hasKey = cachedCodes[hex]; hasKey {
+		return cachedCodes[hex]
+	}
+
+	var average uint64
+	var b uint64
+	var cb uint64
+	var cg uint64
+	var cidx uint64
+	var clrErr float64
+	var cr uint64
+	var g uint64
+	var gidx uint64
+	var grayErr float64
+	var gv uint64
+	var i2cv []uint64
+	var ib uint64
+	var ig uint64
+	var ir uint64
+	var matches [][]string
+	var r uint64
+
+	// For simplicity, assume RGB space is perceptually uniform.
+	// There are 5 places where one of two outputs needs to be
+	// chosen when the input is the exact middle:
+	// - The r/g/b channels and the gray value:
+	//     - the higher value output is chosen
+	// - If the gray and color values have same distance from the
+	//   input
+	//     - color is chosen
+
+	// Calculate the nearest 0-based color index at 16..231
+	r, g, b = 0, 0, 0
+	matches = parseHex.FindAllStringSubmatch(hex, -1)
+	for _, match := range matches {
+		r, _ = strconv.ParseUint(match[1], 16, 8)
+		g, _ = strconv.ParseUint(match[2], 16, 8)
+		b, _ = strconv.ParseUint(match[3], 16, 8)
+	}
+
+	// 0..5 each
+	ir = (r - 35) / 40
+	if r < 48 {
+		ir = 0
+	} else if r < 115 {
+		ir = 1
+	}
+	ig = (g - 35) / 40
+	if g < 48 {
+		ig = 0
+	} else if g < 115 {
+		ig = 1
+	}
+	ib = (b - 35) / 40
+	if b < 48 {
+		ib = 0
+	} else if b < 115 {
+		ib = 1
+	}
+
+	// 0..215 lazy evaluation
+	cidx = (36 * ir) + (6 * ig) + ib + 16
+
+	// Calculate the nearest 0-based gray index at 232..255
+	average = (r + g + b) / 3
+
+	// 0..23
+	gidx = (average - 3) / 10
+	if average > 238 {
+		gidx = 23
+	}
+
+	// Calculate the represented colors back from the index
+	i2cv = []uint64{0, 0x5f, 0x87, 0xaf, 0xd7, 0xff}
+
+	// r/g/b 0..255 each
+	cr = i2cv[ir]
+	cg = i2cv[ig]
+	cb = i2cv[ib]
+
+	// same value for r/g/b 0..255
+	gv = (10 * gidx) + 8
+
+	// Return the one which is nearer to the original rgb values
+	clrErr = math.Pow(float64(cr-r), 2) +
+		math.Pow(float64(cg-g), 2) + math.Pow(float64(cb-b), 2)
+	grayErr = math.Pow(float64(gv-r), 2) +
+		math.Pow(float64(gv-g), 2) + math.Pow(float64(gv-b), 2)
+
+	if clrErr <= grayErr {
+		cachedCodes[hex] = Sprintf("color_%03d", cidx)
+	} else {
+		cachedCodes[hex] = Sprintf("color_%03d", gidx+232)
+	}
+
+	return cachedCodes[hex]
 }
 
 // Hilight will add the appropriate ANSI code to the specified
@@ -43,7 +146,7 @@ func Hilight(code string, str string) string {
 			// Check if hex color code
 			matches = hexCode.FindAllStringSubmatch(code, -1)
 			for _, match := range matches {
-				clr = hexToXterm256(match[2])
+				clr = HexToXterm256(match[2])
 				if strings.HasPrefix(code, "on_") {
 					clr = "on_" + clr
 				}
@@ -91,7 +194,7 @@ func IsDisabled() bool {
 // OnHex will take a hex color code and add the appropriate ANSI color
 // codes to the provided string.
 func OnHex(hex string, str string) string {
-	return colorize("on_"+hexToXterm256(hex), str)
+	return colorize("on_"+HexToXterm256(hex), str)
 }
 
 // OnRainbow will add multiple ANSI color codes to a string for a
