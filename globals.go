32c39e5f1c2872414f5d867b2092904289f7ee2e
Miles Whittaker <mj@whitta.dev>
2022-08-03 22:30:18 -0400

    Updated deps

--- globals.go
+++ globals.go
@@ -88,7 +88,7 @@ var Modes = map[string]string{
 }
 
 // Version is the package version
-const Version = "1.11.0"
+const Version = "1.11.1"
 
 // Various regular expressions
 var allCodes = regexp.MustCompile(`\x1b\[([0-9;]*m|K)`)
